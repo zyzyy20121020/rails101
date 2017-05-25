244c8d9f9fdbe151fc618f9116faadf8a05414a5
ReadingPig <zyzpop830203@163.com>
2017-05-26 01:13:02 +0800

    implement my groups again

---
+++ app/controllers/account/groups_controller.rb
@@ -0,0 +1,5 @@
+class Account::GroupsController < ApplicationController
+  def index
+    @groups = current_user.participated_groups
+  end
+end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,11 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy,
+                :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy,
                 :join, :quit]
 
   def index
-    @groups = Group.all
+    @groups = current_user.participated_groups
   end
 
   def show
